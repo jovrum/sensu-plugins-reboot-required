786aad8e537229f04e7f1199ada376ba2ba89a26
Jakob Øvrum <jovrum@cisco.com>
2017-11-15 14:32:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Sensu-Plugins-reboot-required
+
+## Functionality
+
+## Files
+ * bin/check-reboot-required.rb
+
+
+## Usage
+
+## Installation
+
+[Installation and Setup](http://sensu-plugins.io/docs/installation_instructions.html)
+
+## Notes
---
+++ bin/check-reboot-required.rb
@@ -0,0 +1,59 @@
+#! /usr/bin/env ruby
+# encoding: UTF-8
+#
+# check-reboot-required
+#
+# DESCRIPTION:
+# Check if a reboot was required by a package upgrade on a Debian-based OS.
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#
+# USAGE:
+#
+# NOTES:
+#
+
+require 'sensu-plugin/check/cli'
+
+class CheckRebootRequired < Sensu::Plugin::Check::CLI
+  option :ignore_packages,
+         short: '-i FOO',
+         long: '--ignore-packages pkg1[,pkg2,...,pkgN]',
+         description: 'Packages requesting reboot to ignore',
+         default: ''
+
+  def package_ignored?(pkg)
+    @ignored_package_patterns ||= config[:ignore_packages].split(',')
+    @ignored_package_patterns.any? { |pattern| File.fnmatch(pattern, pkg) }
+  end
+
+  def run
+    if File.file?('/var/run/reboot-required')
+      reason = File.read('/var/run/reboot-required').chomp
+      reason = 'reboot required' if reason.empty?
+
+      pkglist_path = '/var/run/reboot-required.pkgs'
+      if File.file?(pkglist_path)
+        packages = File.readlines(pkglist_path)
+                       .map(&:chomp)
+                       .reject(&method(:package_ignored?))
+
+        if packages.empty?
+          ok # Reboot required only by ignored packages
+        else
+          warning("reboot required by packages: #{packages.join(', ')}")
+        end
+      else
+        warning(reason) # Reboot required by unknown packages
+      end
+    end
+    ok
+  end
+end
